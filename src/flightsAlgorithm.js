ca1c89536f1b13a2d20fc24bb6eb164bc92e482d
Yurii Romanov <yurii.romanov@tdsoft.pl>
2019-05-16 16:54:46 +0200

    Algorithm updated

--- src/flightsAlgorithm.js
+++ src/flightsAlgorithm.js
@@ -28,43 +28,48 @@ const
     convertCurrency = (price, currency) => {
         switch (currency) {
             case 'EUR':
-                currency =  4.29;
+                currency = 4.29;
                 break;
             case 'CZK':
-                currency =  0.17;
+                currency = 0.17;
                 break;
             case 'BGN':
-                currency =  2.19;
+                currency = 2.19;
                 break;
             case 'PLN':
-                currency =  1;
+                currency = 1;
                 break;
             case 'DKK':
-                currency =  0.58;
+                currency = 0.58;
                 break;
             case 'GBP':
-                currency =  4.99;
+                currency = 4.99;
                 break;
             case 'HRK':
-                currency =  0.58;
+                currency = 0.58;
                 break;
             case 'HUF':
-                currency =  0.013;
+                currency = 0.013;
                 break;
             case 'RON':
-                currency =  0.90;
+                currency = 0.90;
                 break;
             case 'SEK':
-                currency =  0.40;
+                currency = 0.40;
                 break;
         }
         return (price * currency).toFixed(2)
-    },
+    }, //budget -  how much you can spend on one ticket
+
+    //https://desktopapps.ryanair.com/v4/en-gb/availability?ADT=1&CHD=0&DateIn=2019-06-28&DateOut=2019-06-28&Destination=DUB&FlexDaysIn=6&FlexDaysOut=2&INF=0&IncludeConnectingFlights=true&Origin=LCJ&RoundTrip=true&TEEN=0&ToUs=AGREED&exists=false
     getTickets = async (budget, dateTo, dateFrom, destination, origin) => {
         const
             ticketsTo = [],
             ticketsBack = [],
-            url = 'https://desktopapps.ryanair.com/v4/en-gb/availability?ADT=1&CHD=0&DateIn=2019-05-22&DateOut=2019-05-16&Destination=CGN&FlexDaysIn=0&FlexDaysOut=0&INF=0&IncludeConnectingFlights=false&Origin=SXF&RoundTrip=true&TEEN=0&ToUs=AGREED&exists=false',
+            url = 'https://desktopapps.ryanair.com/v4/en-gb/availability?ADT=1&CHD=0&DateIn=' + dateTo + '&DateOut=' +
+                dateFrom + '&Destination=' + destination +
+                '&FlexDaysIn=0&FlexDaysOut=0&INF=0&IncludeConnectingFlights=false&Origin=' + origin +
+                '&RoundTrip=true&TEEN=0&ToUs=AGREED&exists=false'
         ;
         let currency = '';
 
@@ -80,8 +85,9 @@ const
                 ticket.arr = moment(flight.time[1]).format('HH:mm');
                 ticket.duration = flight.duration;
                 ticket.price = convertCurrency(flight.regularFare.fares[0].amount, currency);
-
-                ticketsTo.push(ticket);
+                if (budget >= ticket.price) {
+                    ticketsTo.push(ticket);
+                }
             });
 
             //Get options for trip from destination
@@ -91,10 +97,12 @@ const
                 ticket.arr = moment(flight.time[1]).format('HH:mm');
                 ticket.duration = flight.duration;
                 ticket.price = convertCurrency(flight.regularFare.fares[0].amount, currency);
+                if (budget >= ticket.price) {
+                    ticketsBack.push(ticket);
+                }
 
-                ticketsBack.push(ticket);
             });
-        })
+        });
 
         return {
             ticketsTo,
